fba3ac7c7dd395f4eb389d5b4fe4af514124d10a
phantuanminh <phantuanminh2002@gmail.com>
2021-05-20 03:54:01 +0700

    Finished add_first and add_end. Solved CORS issue. TODO: Clean code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,23 @@
 import React, { useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ChallengeForm } from './components/form';
+import { Container } from "semantic-ui-react";
 
 function App() {
-  useEffect(() => {
-    fetch('/api').then(response =>
-      response.json().then(data => {
-        console.log(data);
-      })
-      );
-  }, []);
+  // useEffect(() => {
+  //   fetch('/api').then(response =>
+  //     response.json().then(data => {
+  //       console.log(data);
+  //     })
+  //     );
+  // }, []);
 
   return (
     <div className="App">
-      hello
+      <Container>
+        <ChallengeForm />
+      </Container>
     </div>
   );
 }
--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Form } from "semantic-ui-react";
+import { Form, Input, Button } from "semantic-ui-react";
 
-export const Form = () => {
-  const [name, num] = useState("");
+const ChallengeForm = () => {
+  const [name, setName] = useState("");
+  const [num, setNum] = useState("");
 
   return (
     <Form>
@@ -10,14 +11,14 @@ export const Form = () => {
         <Input
           placeholder="name"
           value={name}
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(e) => setName(e.target.value)}
         />
       </Form.Field>
       <Form.Field>
-        <Number
+        <Input
           placeholder="number"
           value={num}
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(e) => setNum(e.target.value)}
         />
       </Form.Field>
       <Form.Field>
@@ -25,19 +26,32 @@ export const Form = () => {
           onClick={async () => {
             const data = { name, num };
             const response = await fetch("/add_first", {
-              method: "GET",
+              crossDomain: true,
+              method: "POST",
+              mode: "cors",
+              cache: "no-cache",
+              credentials: "same-origin",
               headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Methods": "OPTIONS, GET, POST",
                 "Content-Type": "application/json",
               },
-              body: JSON.stringify(),
+              body: JSON.stringify(data),
             });
 
             if (response.ok) {
               console.log("respond work");
+              const data = await response.json();
+              console.log(data);
             }
           }}
-        ></Button>
+        >
+          {" "}
+          Submit{" "}
+        </Button>
       </Form.Field>
     </Form>
   );
 };
+
+export { ChallengeForm };
